31cbc80512de6467da762bf442f1f21b84f4e992
friedhelmensch <ffnoname@gmx.de>
2017-11-07 14:26:33 +0100

    initial version

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+fs = require('fs'),
+PDFParser = require("pdf2json");
+var http = require('http');
 var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function (request, response) {
+  var file = fs.createWriteStream("./WochenKarte2.pdf");
+  var request = http.get("http://www.restaurant-tresor.de/index_htm_files/Wochenkarte.pdf", function (responsePdf) {
+    var stream = responsePdf.pipe(file);
+    stream.on("finish", function () {
+      let pdfParser = new PDFParser(this, 1);
+      pdfParser.on("pdfParser_dataError", errData => {
+        console.error(errData.parserError)
+      }
+      );
+      pdfParser.on("pdfParser_dataReady", function(pdfData) {
+        //console.log(pdfParser.getRawTextContent());
+        response.send(pdfParser.getRawTextContent())
+      });
+      pdfParser.loadPDF("./WochenKarte2.pdf");
+    })
+  });
 })
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
