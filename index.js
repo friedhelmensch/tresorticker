7f6eb2e863fd177efa2c055b9f16eaca3b04cf9d
friedhelmensch <ffnoname@gmx.de>
2017-11-07 16:39:00 +0100

    added post

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+app.post('/', function (request, response) {
+  var file = fs.createWriteStream("./WochenKarte2.pdf");
+  var request = http.get("http://www.restaurant-tresor.de/index_htm_files/Wochenkarte.pdf", function (responsePdf) {
+    var stream = responsePdf.pipe(file);
+    stream.on("finish", function () {
+      var pdfParser = new PDFParser(this, 1);
+      pdfParser.on("pdfParser_dataError", errData => {
+        console.error(errData.parserError)
+      }
+      );
+      pdfParser.on("pdfParser_dataReady", function(pdfData) {
+        //console.log(pdfParser.getRawTextContent());
+        response.send(pdfParser.getRawTextContent())
+      });
+      pdfParser.loadPDF("./WochenKarte2.pdf");
+    })
+  });
+})
+
 app.get('/', function (request, response) {
   var file = fs.createWriteStream("./WochenKarte2.pdf");
   var request = http.get("http://www.restaurant-tresor.de/index_htm_files/Wochenkarte.pdf", function (responsePdf) {
