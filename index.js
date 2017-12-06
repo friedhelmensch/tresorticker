6a32412c5fd535a1dcf3398dbd266bbf4b9a6412
ffnoname <ffnoname@gmx.de>
2017-12-06 23:48:34 +0100

    change to fetch and buffer

--- index.js
+++ index.js
@@ -1,43 +1,16 @@
-fs = require('fs');
-PDFParser = require("pdf2json");
-var http = require('http');
+const fetch = require("node-fetch");
 var express = require('express');
 var app = express();
 const helper = require('./helper');
+const executer = require('./executer');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.post('/', function (request, response) {
-  var file = fs.createWriteStream("./WochenKarte2.pdf");
-  var request = http.get("http://www.restaurant-tresor.de/index_htm_files/Wochenkarte.pdf", function (responsePdf) {
-    var stream = responsePdf.pipe(file);
-    stream.on("finish", function () {
-      var pdfParser = new PDFParser(this, 1);
-      pdfParser.on("pdfParser_dataError", errData => {
-        response.send(JSON.stringify({
-          "response_type": "ephemeral",
-          "text": errData.parserError
-        }));
-        console.error(errData.parserError)
-      }
-      );
-      pdfParser.on("pdfParser_dataReady", function (pdfData) {
+  
+  executer.execute(fetch, response, helper);
 
-        var pdfAsText = pdfParser.getRawTextContent();
-        var menu = helper.getMenu(pdfAsText);
-        var today = helper.getDay(new Date().getDay());
-        var todaysMeals = helper.extract(menu, today);
-
-        response.setHeader('Content-Type', 'application/json');
-        response.send(JSON.stringify({
-          "response_type": "in_channel",
-          "text": todaysMeals
-        }));
-      });
-      pdfParser.loadPDF("./WochenKarte2.pdf");
-    })
-  });
 })
 
 app.listen(app.get('port'), function () {
