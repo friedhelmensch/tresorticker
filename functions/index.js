5bfa9dfb083d68a3826e6cd2837f2217997f1677
Hannes Rang <hannes@rang.de>
2019-04-06 20:22:57 +0200

    comments about formatting for teams

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,13 @@ exports.tresorMenu = functions.https.onRequest(async (request, response) => {
     try {
         const todaysMeals = await menuProvider.getTodaysMeals(fetch);
 
+        //Slack formatting
         let message = "Heute beim Tresor: \n\n";
         todaysMeals.map(x => x.meals.map(m => message += m + '\n'))
+        
+        //Microsoft Teams formatting
+        //TODO: find out how Cards have to be formatted and how the response needs to look like 
+        //in order for teams to properly show the menu
 
         response.send({
             type: "message",
