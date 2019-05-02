b7c78a81cd7aa99fd8d6aa98779eaabcff65d76c
Hannes <hannes@rang.de>
2019-05-02 14:29:59 +0200

    fixing bug where week started on sunday

--- functions/lib/menuProvider.js
+++ functions/lib/menuProvider.js
@@ -1,93 +1,147 @@
 const PDFParser = require("pdf2json");
-const LocalDate = require('js-joda').LocalDate;
+const LocalDate = require("js-joda").LocalDate;
 
-exports.getTodaysMeals = async function (fetch) {
-    const menuResponse = await fetch("http://www.restaurant-tresor.de/index_htm_files/Wochenkarte.pdf");
+exports.getTodaysMeals = async function(fetch) {
+    const menuResponse = await fetch(
+        "http://www.restaurant-tresor.de/index_htm_files/Wochenkarte.pdf"
+    );
     const menuBuffer = await menuResponse.buffer();
     const pdfAsText = await getPdfAsText(menuBuffer);
     const todaysMeals = exports.getMenuByDay(LocalDate.now(), pdfAsText);
     return todaysMeals;
-}
+};
 
-exports.getMenuByDay = function (date, textMenu) {
-    const weekleMenu = exports.getMenu(textMenu, date.year());
-    const todaysMenu = weekleMenu.filter(f => f.day === date.toString() | f.day === 'Täglich')
+exports.getMenuByDay = function(date, textMenu) {
+    const weeklyMenu = exports.getMenu(textMenu, date.year());
+
+    const todaysMenu = weeklyMenu.filter(
+        f => (f.day === date.toString()) | (f.day === "Täglich")
+    );
     return todaysMenu;
-}
+};
 
-exports.getDayAndMonth = function (mealsText) {
-    const weekRangeRegEx = /\d{1,2}.(| )(Januar?|Februar?|März?|April?|Mai|Juni?|Juli?|August?|September?|Oktober?|November?|Dezember?) bis/g
+exports.getDayAndMonth = function(mealsText) {
+    const weekRangeRegEx = /\d{1,2}.(| )(Januar?|Februar?|März?|April?|Mai|Juni?|Juli?|August?|September?|Oktober?|November?|Dezember?) bis/g;
 
     const dayAndMonth = mealsText
         .match(weekRangeRegEx)[0]
-        .replace('bis', '')
-        .replace(' ', '')
+        .replace("bis", "")
+        .replace(" ", "")
         .trim()
-        .split('.')
+        .split(".");
 
     return dayAndMonth;
-}
+};
 
-exports.getMenu = function (textMenu, year) {
-
+exports.getMenu = function(textMenu, year) {
     //tests provide year because test data is static
     if (!year) year = new Date().getFullYear();
 
     const mealsText = textMenu
-        .replace(/(\r\n |\n |\r )/gm, '')
-        .replace('Wochenkarte', '')
-        .replace('----------------Page (0) Break----------------', '')
+        .replace(/(\r\n |\n |\r )/gm, "")
+        .replace("Wochenkarte", "")
+        .replace("----------------Page (0) Break----------------", "")
         .trim();
 
-    const allSeperators = ['Wochenkarte', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Täglich']
+    const allSeperators = [
+        "Wochenkarte",
+        "Montag",
+        "Dienstag",
+        "Mittwoch",
+        "Donnerstag",
+        "Freitag",
+        "Täglich"
+    ];
 
-    const menuSections = mealsText.split(new RegExp(allSeperators.join('|'), 'g')).slice(1)
+    const menuSections = mealsText
+        .split(new RegExp(allSeperators.join("|"), "g"))
+        .slice(1)
+        .map(section =>
+            section
+                .trim()
+                .split(/(\r\n|\n|\r)/gm)
+                .filter(r => r !== "\r\n")
+                .filter(r => r !== "\n")
+        );
 
-    const menuSectionByDay = allSeperators.slice(1)
+    const menuSectionByDay = allSeperators.slice(1);
 
     const dayAndMonth = exports.getDayAndMonth(mealsText);
 
-    const monthNames = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+    const monthNames = [
+        "Januar",
+        "Februar",
+        "März",
+        "April",
+        "Mai",
+        "Juni",
+        "Juli",
+        "August",
+        "September",
+        "Oktober",
+        "November",
+        "Dezember"
+    ];
 
-    const firstDayInMenu = LocalDate.of(year, monthNames.indexOf(dayAndMonth[1].trim()) + 1, dayAndMonth[0]);
+    let firstDayInMenu = LocalDate.of(
+        year,
+        monthNames.indexOf(dayAndMonth[1].trim()) + 1,
+        dayAndMonth[0]
+    );
+
+    if (firstDayInMenu.dayOfWeek().name() === "SUNDAY") {
+        //trying to circumvent human mistakes in the menu.
+        //in the week from 29.04.2019 to 03.05.2019 there was a mistake where the
+        //menu stated to start on the 28.04 (which is a sunday and wrong).
+        firstDayInMenu = firstDayInMenu.plusDays(1);
+    }
+
     const firstDayOfMenuWeek = firstDayInMenu.minusDays(firstDayInMenu.dayOfWeek().ordinal());
 
     const weeklyMenu = menuSectionByDay.reduce((menu, current, idx) => {
-        const sectionMenu = menuSections[idx].trim()
-            .split(/(\r\n|\n|\r)/gm)
-            .filter(r => r !== '\r\n')
-            .filter(r => r !== '\n')
-            .map(m => {
-                const menuPriceMap = m.split('€')
-                let menuText = m;
-                if (menuPriceMap[0] !== undefined && menuPriceMap[1] !== undefined) {
-                    menuText = menuPriceMap[0].trim() + ' € ' + parseFloat(menuPriceMap[1].replace(/,/, '.')).toFixed(2).replace('.', ',');
-                }
+        const menuSection = menuSections[idx];
 
-                return menuText;
-            })
+        const sectionMenu = menuSection.map(m => {
+            const menuPriceMap = m.split("€");
+            let menuText = m;
+            if (
+                menuPriceMap[0] !== undefined &&
+                menuPriceMap[1] !== undefined
+            ) {
+                menuText =
+                    menuPriceMap[0].trim() +
+                    " € " +
+                    parseFloat(menuPriceMap[1].replace(/,/, "."))
+                        .toFixed(2)
+                        .replace(".", ",");
+            }
 
-        const day = current !== 'Täglich' ? firstDayOfMenuWeek.plusDays(idx).toString() : 'Täglich'
-        menu.push({ 'day': day, 'meals': sectionMenu })
+            return menuText;
+        });
+
+        const day =
+            current !== "Täglich"
+                ? firstDayOfMenuWeek.plusDays(idx).toString()
+                : "Täglich";
+        menu.push({ day: day, meals: sectionMenu });
 
         return menu;
     }, []);
 
     return weeklyMenu;
-}
+};
 
-getPdfAsText = async function (menuBuffer) {
-
-    return new Promise(function (resolve, reject) {
+getPdfAsText = async function(menuBuffer) {
+    return new Promise(function(resolve, reject) {
         var pdfParser = new PDFParser(this, 1);
-        pdfParser.on("pdfParser_dataError", (errData) => {
+        pdfParser.on("pdfParser_dataError", errData => {
             reject(errData);
         });
 
-        pdfParser.on("pdfParser_dataReady", (pdfData) => {
+        pdfParser.on("pdfParser_dataReady", pdfData => {
             var pdfAsText = pdfParser.getRawTextContent();
             resolve(pdfAsText);
         });
         pdfParser.parseBuffer(menuBuffer);
     });
-}
+};
--- functions/test/menuProvider.test.js
+++ functions/test/menuProvider.test.js
@@ -1,163 +1,224 @@
-'use strict'
+"use strict";
 
 const menuProvider = require("../lib/menuProvider");
-const expect = require('chai').expect
+const expect = require("chai").expect;
 
-it('Parsing the text representation of Wochenkarte should create menu', () => {
-  const fs = require('fs')
-
-  const menuText = fs.readFileSync('./test/Wochenkarte.content.txt', 'utf8').replace(/(\r\n |\n |\r )/gm, "")
-  const menu = menuProvider.getMenu(menuText, 2018)
-
-  const expectedMenu = [{
-      "day": "2018-02-26",
-      "meals": [
-        "Schupfnudelpfanne mit Sauerkraut und Kasseler € 7,50"
-      ]
-    },
-    {
-      "day": "2018-02-27",
-      "meals": [
-        "Gebratene Hähnchenbrust auf Gemüsereis und Magodip € 7,50"
-      ]
-    },
-    {
-      "day": "2018-02-28",
-      "meals": [
-      "Gefüllter Schweinehals mit Spätzle € 7,50"
-      ]
-    },
-    {
-      "day": "2018-03-01",
-      "meals": [
-        "Überbackenes Putensteak mit Tomate-Mozzarella und Wedges € 7,50"
-      ]
-    },
-    {
-      "day": "2018-03-02",
-      "meals": [
-        "Tilapiafilet mit Rosmarinkartoffeln € 7,50"
-      ]
-    },
-    {
-      "day": "Täglich",
-      "meals": [
-         "Salatteller mit Schinken und Ei € 7,20"
-        ,"Spaghetti „Amatriciana“ mit Speck in Tomatensosse € 7,00"
-        ,"Linseneintopf mit Saiten € 6,80"
-      ]
-    }
-  ];
-
-  expect(menu).to.deep.equal(expectedMenu);
-})
-
-it('GetMenu should return daily special as well as all week daily dishes', () => {
-  const fs = require('fs')
-  const LocalDate = require('js-joda').LocalDate;
-
-  const menuText = fs.readFileSync('./test/Wochenkarte.content.txt', 'utf8').replace(/(\r\n |\n |\r )/gm, "")
-  const todaysMenu = menuProvider.getMenuByDay(LocalDate.of(2018, 2, 26), menuText)
-
-  const expectedDailyMenu = [{
-      "day": "2018-02-26",
-      "meals": [
-        "Schupfnudelpfanne mit Sauerkraut und Kasseler € 7,50"
-      ]
-    },
-    {
-      "day": "Täglich",
-      "meals": [
-        "Salatteller mit Schinken und Ei € 7,20"
-        ,"Spaghetti „Amatriciana“ mit Speck in Tomatensosse € 7,00"
-        ,"Linseneintopf mit Saiten € 6,80"
-      ]
-    }
-  ]
-
-  expect(todaysMenu).to.deep.equal(expectedDailyMenu);
-})
-
-it('When menus week range is not complete get menu should return the right menu based on the remaining dates', () => {
-  const fs = require('fs')
-  const LocalDate = require('js-joda').LocalDate;
-
-  const menuText = fs.readFileSync('./test/Wochenkarte.DateRange.content.txt', 'utf8').replace(/(\r\n |\n |\r )/gm, "")
-  const todaysMenu = menuProvider.getMenuByDay(LocalDate.of(2018, 4, 5), menuText)
-
-  const expectedDailyMenu = [{
-      "day": "2018-04-05",
-      "meals": [
-        "Putenrollbraten mit Spätzle € 7,50"
-      ]
-    },
-    {
-      "day": "Täglich",
-      "meals": [
-        "Salatteller mit Räucherlachs 7,80"
-        ,"Penne Arrabiata mit Fetakäse € 7,20"
-        ,"Rehbraten mit Kartoffelknödel und Wirsingrahm € 15,50"
-      ]
-    }
-  ]
-
-  expect(todaysMenu).to.deep.equal(expectedDailyMenu);
-})
+const monthNames = [
+    "Januar",
+    "Februar",
+    "März",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember"
+];
 
 
-it('GetMenu should in weeks with holidays', () => {
-  const fs = require('fs')
-  const LocalDate = require('js-joda').LocalDate;
+it("Parsing the text representation of Wochenkarte should create menu", () => {
+    const fs = require("fs");
 
-  const menuText = fs.readFileSync('./test/Wochenkarte.Feiertag.content.txt', 'utf8').replace(/(\r\n |\n |\r )/gm, "")
-  const todaysMenu = menuProvider.getMenuByDay(LocalDate.of(2018, 3, 30), menuText)
+    const menuText = fs
+        .readFileSync("./test/Wochenkarte.content.txt", "utf8")
+        .replace(/(\r\n |\n |\r )/gm, "");
+    const menu = menuProvider.getMenu(menuText, 2018);
 
-  const expectedDailyMenu = [{
-      "day": "2018-03-30",
-      "meals": [
-        "Feiertag"
-      ]
-    },
-    {
-      "day": "Täglich",
-      "meals": [
-        "Salatteller mit Mozzarellastiks 7,00"
-        ,"Gebratener Leberkäse mit Zwiebelsosse und Pommes € 7,20"
-        ,"Sauerbraten mit Spätzle und Blaukraut € 13,50"
-      ]
-    }
-  ]
+    const expectedMenu = [
+        {
+            day: "2018-02-26",
+            meals: ["Schupfnudelpfanne mit Sauerkraut und Kasseler € 7,50"]
+        },
+        {
+            day: "2018-02-27",
+            meals: ["Gebratene Hähnchenbrust auf Gemüsereis und Magodip € 7,50"]
+        },
+        {
+            day: "2018-02-28",
+            meals: ["Gefüllter Schweinehals mit Spätzle € 7,50"]
+        },
+        {
+            day: "2018-03-01",
+            meals: [
+                "Überbackenes Putensteak mit Tomate-Mozzarella und Wedges € 7,50"
+            ]
+        },
+        {
+            day: "2018-03-02",
+            meals: ["Tilapiafilet mit Rosmarinkartoffeln € 7,50"]
+        },
+        {
+            day: "Täglich",
+            meals: [
+                "Salatteller mit Schinken und Ei € 7,20",
+                "Spaghetti „Amatriciana“ mit Speck in Tomatensosse € 7,00",
+                "Linseneintopf mit Saiten € 6,80"
+            ]
+        }
+    ];
 
-  expect(todaysMenu).to.deep.equal(expectedDailyMenu);
-})
+    expect(menu).to.deep.equal(expectedMenu);
+});
 
-it('Extracting start date of weekly menu should return 26.02.2018', () => {
-  const fs = require('fs')
-  const LocalDate = require('js-joda').LocalDate;
+it("GetMenu should return daily special as well as all week daily dishes", () => {
+    const fs = require("fs");
+    const LocalDate = require("js-joda").LocalDate;
 
-  const menuText = fs.readFileSync('./test/Wochenkarte.content.txt', 'utf8').replace(/(\r\n |\n |\r )/gm, "")
-  
-  const dayAndMonth =  menuProvider.getDayAndMonth(menuText);
-  const monthNames = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+    const menuText = fs
+        .readFileSync("./test/Wochenkarte.content.txt", "utf8")
+        .replace(/(\r\n |\n |\r )/gm, "");
+    const todaysMenu = menuProvider.getMenuByDay(
+        LocalDate.of(2018, 2, 26),
+        menuText
+    );
 
-  const startDate = LocalDate.of(2018, monthNames.indexOf(dayAndMonth[1].trim()) + 1, dayAndMonth[0])
-  const expectedStartDate = LocalDate.of(2018, 2, 26)
+    const expectedDailyMenu = [
+        {
+            day: "2018-02-26",
+            meals: ["Schupfnudelpfanne mit Sauerkraut und Kasseler € 7,50"]
+        },
+        {
+            day: "Täglich",
+            meals: [
+                "Salatteller mit Schinken und Ei € 7,20",
+                "Spaghetti „Amatriciana“ mit Speck in Tomatensosse € 7,00",
+                "Linseneintopf mit Saiten € 6,80"
+            ]
+        }
+    ];
 
-  expect(expectedStartDate).to.deep.equal(startDate);
-})
+    expect(todaysMenu).to.deep.equal(expectedDailyMenu);
+});
 
-it('Extracting start date of weekly menu should return 02.07.2018 (NoSpaceInDate)', () => {
-  const fs = require('fs')
-  const LocalDate = require('js-joda').LocalDate;
+it("When menus week range is not complete get menu should return the right menu based on the remaining dates", () => {
+    const fs = require("fs");
+    const LocalDate = require("js-joda").LocalDate;
 
-  const menuText = fs.readFileSync('./test/Wochenkarte.NoSpaceInDate.content.txt', 'utf8').replace(/(\r\n |\n |\r )/gm, "")
-  
-  const dayAndMonth =  menuProvider.getDayAndMonth(menuText);
+    const menuText = fs
+        .readFileSync("./test/Wochenkarte.DateRange.content.txt", "utf8")
+        .replace(/(\r\n |\n |\r )/gm, "");
+    const todaysMenu = menuProvider.getMenuByDay(
+        LocalDate.of(2018, 4, 5),
+        menuText
+    );
 
-  const monthNames = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+    const expectedDailyMenu = [
+        {
+            day: "2018-04-05",
+            meals: ["Putenrollbraten mit Spätzle € 7,50"]
+        },
+        {
+            day: "Täglich",
+            meals: [
+                "Salatteller mit Räucherlachs 7,80",
+                "Penne Arrabiata mit Fetakäse € 7,20",
+                "Rehbraten mit Kartoffelknödel und Wirsingrahm € 15,50"
+            ]
+        }
+    ];
 
-  const startDate = LocalDate.of(2018, monthNames.indexOf(dayAndMonth[1].trim()) + 1, dayAndMonth[0])
-  const expectedStartDate = LocalDate.of(2018, 7, 2)
+    expect(todaysMenu).to.deep.equal(expectedDailyMenu);
+});
 
-  expect(expectedStartDate).to.deep.equal(startDate);
-})
+it("GetMenu should in weeks with holidays", () => {
+    const fs = require("fs");
+    const LocalDate = require("js-joda").LocalDate;
+
+    const menuText = fs
+        .readFileSync("./test/Wochenkarte.Feiertag.content.txt", "utf8")
+        .replace(/(\r\n |\n |\r )/gm, "");
+    const todaysMenu = menuProvider.getMenuByDay(
+        LocalDate.of(2018, 3, 30),
+        menuText
+    );
+
+    const expectedDailyMenu = [
+        {
+            day: "2018-03-30",
+            meals: ["Feiertag"]
+        },
+        {
+            day: "Täglich",
+            meals: [
+                "Salatteller mit Mozzarellastiks 7,00",
+                "Gebratener Leberkäse mit Zwiebelsosse und Pommes € 7,20",
+                "Sauerbraten mit Spätzle und Blaukraut € 13,50"
+            ]
+        }
+    ];
+
+    expect(todaysMenu).to.deep.equal(expectedDailyMenu);
+});
+
+it("Extracting start date of weekly menu should return 26.02.2018", () => {
+    const fs = require("fs");
+    const LocalDate = require("js-joda").LocalDate;
+
+    const menuText = fs
+        .readFileSync("./test/Wochenkarte.content.txt", "utf8")
+        .replace(/(\r\n |\n |\r )/gm, "");
+
+    const dayAndMonth = menuProvider.getDayAndMonth(menuText);
+
+    const startDate = LocalDate.of(
+        2018,
+        monthNames.indexOf(dayAndMonth[1].trim()) + 1,
+        dayAndMonth[0]
+    );
+    const expectedStartDate = LocalDate.of(2018, 2, 26);
+
+    expect(expectedStartDate).to.deep.equal(startDate);
+});
+
+it("Extracting start date of weekly menu should return 02.07.2018 (NoSpaceInDate)", () => {
+    const fs = require("fs");
+    const LocalDate = require("js-joda").LocalDate;
+
+    const menuText = fs
+        .readFileSync("./test/Wochenkarte.NoSpaceInDate.content.txt", "utf8")
+        .replace(/(\r\n |\n |\r )/gm, "");
+
+    const dayAndMonth = menuProvider.getDayAndMonth(menuText);
+
+    const startDate = LocalDate.of(
+        2018,
+        monthNames.indexOf(dayAndMonth[1].trim()) + 1,
+        dayAndMonth[0]
+    );
+    const expectedStartDate = LocalDate.of(2018, 7, 2);
+
+    expect(expectedStartDate).to.deep.equal(startDate);
+});
+
+it("should return correct meals even start date is a sunday instead of monday", () => {
+    const fs = require("fs");
+    const LocalDate = require("js-joda").LocalDate;
+
+    const menuText = fs
+        .readFileSync("./test/Wochenkarte.WrongWeekRange.content.txt", "utf8")
+        .replace(/(\r\n |\n |\r )/gm, "");
+
+    const todaysMenu = menuProvider.getMenuByDay(
+        LocalDate.of(2019, 5, 1),
+        menuText
+    );
+
+    const expectedDailyMenu = [
+        {
+            day: "2019-05-01",
+            meals: ["Feiertag"]
+        },
+        {
+            day: "Täglich",
+            meals: [
+                "Salatteller mit gebratenem Pangasiusfilet € 7,60",
+                "Champignonomelett mit Salatbukett € 6,80",
+                "Kalbsbratwurst mit Kartoffelsalat € 7,20"
+            ]
+        }
+    ];
+    expect(todaysMenu).to.deep.equal(expectedDailyMenu);
+});
